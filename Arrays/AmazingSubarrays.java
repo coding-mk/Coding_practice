3e188d6be2dc5469646bcbba71c447a991e97a62
Manish Kshetrapal <120669344+coding-mk@users.noreply.github.com>
2022-12-21 17:11:39 +0530

    Update AmazingSubarrays.java

--- Arrays/AmazingSubarrays.java
+++ Arrays/AmazingSubarrays.java
@@ -1,4 +1,5 @@
 /*
+Problem Description
 You are given a string S, and you have to find all the amazing substrings of S.
 An amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
 
